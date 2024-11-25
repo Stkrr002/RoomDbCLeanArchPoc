4246b6e07816c8f1479b295419b010ca46473394
sumitraaho <sumitkumar@raaho.in>
2024-11-25 18:16:20 +0530

    di done

--- app/src/main/java/com/alpharays/yudemoapplicationlu/di/LocalModule.kt
+++ app/src/main/java/com/alpharays/yudemoapplicationlu/di/LocalModule.kt
@@ -1,15 +1,28 @@
 package com.alpharays.yudemoapplicationlu.di
 
+import android.content.Context
+import androidx.room.Room
+import com.alpharays.yudemoapplicationlu.data.ExpenseDataBase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class LocalModule {
     @Provides
-    fun provideExpenseDataBase(){
-
+    @Singleton
+    fun provideExpenseDataBase(@ApplicationContext context: Context): ExpenseDataBase {
+        return Room.databaseBuilder(
+            context,
+            ExpenseDataBase::class.java, "expense-tracker-app"
+        ).build()
     }
+
+    @Provides
+    @Singleton
+    fun providesExpenseDao(expenseDataBase: ExpenseDataBase) = expenseDataBase.expenseDao()
 }
